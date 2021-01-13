ac10dd7d904042220b77e2f644bf9f4804f87470
Javi <javi_castillo18@hotmail.com>
2021-01-13 10:33:14 +0100

    Add selector for sustantive and verb

--- Learning/Learning/UI/WordsVC/CurrentWordViewController.swift
+++ Learning/Learning/UI/WordsVC/CurrentWordViewController.swift
@@ -9,8 +9,39 @@ import UIKit
 
 class CurrentWordViewController: UIViewController {
 
+    @IBOutlet weak var typeItemSelector: ItemSelector!
+    @IBOutlet weak var sustantiveView: UIView!
+    @IBOutlet weak var verbView: UIView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        prepareScreen()
+    }
+
+    private func prepareScreen() {
+        typeItemSelector.listItems = ["Sustantivo", "Verbo"]
+        typeItemSelector.itemId = .initial
+        typeItemSelector.delegate = self
+    }
+
+}
+
+extension CurrentWordViewController: ItemSelectorDelegate {
+
+    func valueChanged(_ selector: ItemSelector) {
+        switch selector.itemId {
+        case .initial:
+            if selector.currentIndex == 0 {
+                sustantiveView.isHidden = false
+                verbView.isHidden = true
+            } else if selector.currentIndex == 1 {
+                sustantiveView.isHidden = true
+                verbView.isHidden = false
+            }
+        default:
+            break
+        }
+
     }
 
 }
