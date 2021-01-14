4cdd1bba14463fd4dfe4068cbdb1e559ff2a7f6c
Javi <javi_castillo18@hotmail.com>
2021-01-14 10:10:40 +0100

    Create first version of sustantive views

--- Learning/Learning/UI/WordsVC/CurrentWordViewController.swift
+++ Learning/Learning/UI/WordsVC/CurrentWordViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class CurrentWordViewController: UIViewController {
 
+    private var languages: [String] = []
+
     @IBOutlet weak var typeItemSelector: ItemSelector!
     @IBOutlet weak var sustantiveView: UIView!
     @IBOutlet weak var verbView: UIView!
@@ -22,6 +24,59 @@ class CurrentWordViewController: UIViewController {
         typeItemSelector.listItems = ["Sustantivo", "Verbo"]
         typeItemSelector.itemId = .initial
         typeItemSelector.delegate = self
+
+        let dbManager = DBManager(with: "wordsdb.sql")
+        if let availableLanguages = dbManager.query("select * from languages") {
+            for availableLanguage in availableLanguages {
+                if let availableLanguage = availableLanguage as? [String] {
+                    languages.append(availableLanguage[1])
+                }
+            }
+        }
+        prepareSustantiveView()
+
+    }
+
+    private func prepareSustantiveView() {
+        for language in languages {
+            let languageView = UIView()
+            let nameLabel = UILabel()
+            nameLabel.text = language
+            languageView.addSubview(nameLabel)
+            nameLabel.translatesAutoresizingMaskIntoConstraints = false
+            let labelHorizontalConstraints = NSLayoutConstraint.constraints(
+                withVisualFormat: "H:|-0-[nameLabel]-0-|",
+                options: NSLayoutConstraint.FormatOptions(),
+                metrics: nil,
+                views: ["nameLabel": nameLabel])
+            let labelVerticalConstraints = NSLayoutConstraint.constraints(
+                withVisualFormat: "V:|-0-[nameLabel]-0-|",
+                options: NSLayoutConstraint.FormatOptions(),
+                metrics: nil,
+                views: ["nameLabel": nameLabel])
+
+            languageView.addConstraints(labelHorizontalConstraints+labelVerticalConstraints)
+
+            sustantiveView.addSubview(languageView)
+            
+            languageView.translatesAutoresizingMaskIntoConstraints = false
+
+            let horizontalConstraints = NSLayoutConstraint.constraints(
+                withVisualFormat: "H:|-0-[languageView]-0-|",
+                options: NSLayoutConstraint.FormatOptions(),
+                metrics: nil,
+                views: ["languageView": languageView])
+
+            let verticalConstraints = NSLayoutConstraint.constraints(
+                withVisualFormat: "V:|-0-[languageView]-0-|",
+                options: NSLayoutConstraint.FormatOptions(),
+                metrics: nil,
+                views: ["languageView": languageView])
+
+            sustantiveView.addConstraints(horizontalConstraints+verticalConstraints)
+
+
+        }
     }
 
 }
