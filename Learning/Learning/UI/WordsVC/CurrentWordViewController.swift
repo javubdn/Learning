a061b8175c6376fb733b57d2fea50c3d0ecb7964
Javi <javi_castillo18@hotmail.com>
2021-01-20 16:58:02 +0100

    Add gender for sustantive in new word screen

--- Learning/Learning/UI/WordsVC/CurrentWordViewController.swift
+++ Learning/Learning/UI/WordsVC/CurrentWordViewController.swift
@@ -51,13 +51,16 @@ class CurrentWordViewController: UIViewController {
                 metrics: nil,
                 views: ["nameLabel": nameLabel])
             let nameLabelVerticalConstraints = NSLayoutConstraint.constraints(
-                withVisualFormat: "V:|-0-[nameLabel(30)]-0-|",
+                withVisualFormat: "V:|-0-[nameLabel(30)]",
                 options: NSLayoutConstraint.FormatOptions(),
                 metrics: nil,
                 views: ["nameLabel": nameLabel])
 
             let nameTextField = UITextField()
             nameTextField.borderStyle = .roundedRect
+            nameTextField.delegate = self
+            nameTextField.isUserInteractionEnabled = true
+            nameTextField.isEnabled = true
             languageView.addSubview(nameTextField)
             nameTextField.translatesAutoresizingMaskIntoConstraints = false
             let nameTextFieldHorizontalConstraints = NSLayoutConstraint.constraints(
@@ -66,13 +69,45 @@ class CurrentWordViewController: UIViewController {
                 metrics: nil,
                 views: ["nameLabel": nameLabel, "nameTextField": nameTextField])
             let nameTextFieldVerticalConstraints = NSLayoutConstraint.constraints(
-                withVisualFormat: "V:|-0-[nameTextField(30)]-0-|",
+                withVisualFormat: "V:|-0-[nameTextField(30)]",
                 options: NSLayoutConstraint.FormatOptions(),
                 metrics: nil,
                 views: ["nameTextField": nameTextField])
 
+            let genderLabel = UILabel()
+            genderLabel.text = "Género en \(language)"
+            languageView.addSubview(genderLabel)
+            genderLabel.translatesAutoresizingMaskIntoConstraints = false
+            let genderLabelHorizontalConstraints = NSLayoutConstraint.constraints(
+                withVisualFormat: "H:|-0-[genderLabel]",
+                options: NSLayoutConstraint.FormatOptions(),
+                metrics: nil,
+                views: ["genderLabel": genderLabel])
+            let genderLabelVerticalConstraints = NSLayoutConstraint.constraints(
+                withVisualFormat: "V:[nameLabel]-20-[genderLabel(30)]-0-|",
+                options: NSLayoutConstraint.FormatOptions(),
+                metrics: nil,
+                views: ["nameLabel": nameLabel, "genderLabel": genderLabel])
+
+            let genderTextField = UITextField()
+            genderTextField.borderStyle = .roundedRect
+            languageView.addSubview(genderTextField)
+            genderTextField.translatesAutoresizingMaskIntoConstraints = false
+            let genderTextFieldHorizontalConstraints = NSLayoutConstraint.constraints(
+                withVisualFormat: "H:[genderLabel]-20-[genderTextField(100)]-0-|",
+                options: NSLayoutConstraint.FormatOptions(),
+                metrics: nil,
+                views: ["genderLabel": genderLabel, "genderTextField": genderTextField])
+            let genderTextFieldVerticalConstraints = NSLayoutConstraint.constraints(
+                withVisualFormat: "V:[nameTextField]-20-[genderTextField(30)]",
+                options: NSLayoutConstraint.FormatOptions(),
+                metrics: nil,
+                views: ["nameTextField": nameTextField, "genderTextField": genderTextField])
+
             languageView.addConstraints(nameLabelHorizontalConstraints+nameLabelVerticalConstraints)
             languageView.addConstraints(nameTextFieldHorizontalConstraints+nameTextFieldVerticalConstraints)
+            languageView.addConstraints(genderLabelHorizontalConstraints+genderLabelVerticalConstraints)
+            languageView.addConstraints(genderTextFieldHorizontalConstraints+genderTextFieldVerticalConstraints)
 
             sustantiveView.addSubview(languageView)
             
@@ -127,4 +162,15 @@ extension CurrentWordViewController: ItemSelectorDelegate {
 
     }
 
+}
+
+extension CurrentWordViewController: UITextFieldDelegate {
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+
+    }
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 }
